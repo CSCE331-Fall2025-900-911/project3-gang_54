f84e237f47fbaedf045518c9f102e40f31efab44
Ray Chu <rchu7x7@gmail.com>
2025-11-10 12:16:07 -0600

    update url in readme, fix styling of homepage

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -7,6 +7,10 @@ export default function Home() {
         <img src="https://images.squarespace-cdn.com/content/v1/5b7edf5036099bcb6fd1e12e/1535071878205-1TIG23OI2MXB06MULX01/CFD8C5FA-C751-4D78-B203-AA60F6E671D3.png" alt="sharetea logo" />
         <p>Our drinks are made from carefully selected ingredients shipped directly from Taiwan. All products are 100% guaranteed to arrive fresh and tasty. We use high quality tea leaves rather than artificial flavors to produce the best tasting milk tea and fruit drinks. It is our mission to provide the best quality tea to our fans!</p>
       </div>
+      <hr></hr>
+      <div className="box" style={{textAlign:'center', alignItems:'center', justifyContent:'center'}}>
+        <p><strong>Sign Up</strong> or <strong>Login</strong> to start ordering now!</p>
+      </div>
     </div>
   );
 }
