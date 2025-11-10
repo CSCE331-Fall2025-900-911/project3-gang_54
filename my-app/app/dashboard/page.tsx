e6beeceaab73ffd93c3645311f9321b5cc8389b2
Ray Chu <rchu7x7@gmail.com>
2025-11-10 17:33:54 -0600

    button for view menu item

--- my-app/app/dashboard/page.tsx
+++ my-app/app/dashboard/page.tsx
@@ -1,16 +1,7 @@
-// import defined functions from another file
-
 export default function Home() {
-    const managerFunc = () => {
-        console.log("clicked");
-    };
-    return (
-        <main>
-        <div>
-            <h1 style={{color:'white', marginTop:60}}>welcome to the manager's dashboard</h1>
-            <p>hello</p>
-            {/* <button onClick={managerFunc}>Try clicking me to print to console</button> */}
-        </div>
-        </main>
-    )
+  return (
+    <main className="home-container">
+      <button className="button">View Menu Items</button>
+    </main>
+  );
 }
