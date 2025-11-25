282bbd24c49335810039746c87e13f9dc8659843
vedbulusu <vedbuls@tamu.edu>
2025-11-24 20:02:18 -0600

    updated route.ts

--- my-app/app/api/ordermenu/route.ts
+++ my-app/app/api/ordermenu/route.ts
@@ -1,1 +1,28 @@
+import { NextRequest, NextResponse } from "next/server";
+import { Client } from "pg";
+
+
+export async function GET() {
+  const client = new Client({
+    user: process.env.DB_user,
+    host: process.env.DB_host,
+    database: process.env.DB_name,
+    password: process.env.DB_password,
+    port: 5432,
+  });
+
+  try {
+    await client.connect();
+    const result = await client.query("SELECT * FROM menu_items");
+    return NextResponse.json(result.rows);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  } finally {
+    await client.end();
+  }
+}
 
